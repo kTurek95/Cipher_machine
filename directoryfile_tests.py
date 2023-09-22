778ce62e83193626abda265d9188552d2a288b82
kTurek95 <k.turek1995@gmail.com>
2023-09-22 22:22:53 +0100

    module with test for directoryfile

---
+++ directoryfile_tests.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+import unittest
+from directoryfile import DirectoryFile
+
+
+class TestDirectoryFile(unittest.TestCase):
+    def setUp(self):
+        self.temp_dir = 'temp_test_dir'
+        os.mkdir(self.temp_dir)
+        with open(os.path.join(self.temp_dir, 'file1.txt'), 'w', encoding='UTF-8') as file:
+            file.write("Example content")
+        os.mkdir(os.path.join(self.temp_dir, 'temp_dir2'))
+        with open(os.path.join(self.temp_dir, 'temp_dir2', 'file2.txt'), 'w', encoding='UTF-8')\
+                as file:
+            file.write("Another example")
+
+    def tearDown(self):
+        shutil.rmtree(self.temp_dir)
+
+    def test_get_file(self):
+        dir_fil = DirectoryFile(self.temp_dir)
+        result = dir_fil.get_file()
+
+        self.assertIn(os.path.join(self.temp_dir, 'file1.txt'), result)
+        self.assertIn(os.path.join(self.temp_dir, 'temp_dir2', 'file2.txt'), result)
+
+    def test_text_from_file(self):
+        dir_fil = DirectoryFile(self.temp_dir)
+        result = dir_fil.text_from_file()
+
+        self.assertIn("Example content", result)
+        self.assertIn("Another example", result)
+
+if __name__ == '__main__':
+    unittest.main()
