adfa9e084ea7bbb26e4fb8e4397c50dec8b61bd1
kTurek95 <k.turek1995@gmail.com>
2023-09-25 22:43:15 +0100

    adding docstrings

--- directoryfile_tests.py
+++ directoryfile_tests.py
@@ -1,3 +1,5 @@
+""" Module with tests for Directoryfile class"""
+
 import os
 import shutil
 import unittest
@@ -5,7 +7,16 @@ from directoryfile import DirectoryFile
 
 
 class TestDirectoryFile(unittest.TestCase):
+    """
+    Test suite for the DirectoryFile class functionalities.
+    """
     def setUp(self):
+        """
+        Set up the testing environment before each test.
+
+        Creates a temporary directory and files
+        for later testing the methods of the DirectoryFile class.
+        """
         self.temp_dir = 'temp_test_dir'
         os.mkdir(self.temp_dir)
         with open(os.path.join(self.temp_dir, 'file1.txt'), 'w', encoding='UTF-8') as file:
@@ -16,9 +27,16 @@ class TestDirectoryFile(unittest.TestCase):
             file.write("Another example")
 
     def tearDown(self):
+        """
+        Clean up the testing environment after each test, removing temporary files and directories.
+        """
         shutil.rmtree(self.temp_dir)
 
     def test_get_file(self):
+        """
+        Checks if the method correctly lists all files
+        in the specified directory and its subdirectories.
+        """
         dir_fil = DirectoryFile(self.temp_dir)
         result = dir_fil.get_file()
 
@@ -26,6 +44,10 @@ class TestDirectoryFile(unittest.TestCase):
         self.assertIn(os.path.join(self.temp_dir, 'temp_dir2', 'file2.txt'), result)
 
     def test_text_from_file(self):
+        """
+        Verifies if the method correctly retrieves text content
+        from files in the specified directory.
+        """
         dir_fil = DirectoryFile(self.temp_dir)
         result = dir_fil.text_from_file()
 
@@ -33,6 +55,10 @@ class TestDirectoryFile(unittest.TestCase):
         self.assertIn("Another example", result)
 
     def test_append_text_to_file(self):
+        """
+        Ensures that the method appends the provided text content
+        to the specified file in the directory.
+        """
         dir_fil = DirectoryFile(self.temp_dir)
         dir_fil.append_text_to_file('New content', 'file1.txt')
 
