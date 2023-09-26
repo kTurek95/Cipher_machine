fab8e1b23ecfe5074babb5d59e7af1dbd8c9ac0d
kTurek95 <k.turek1995@gmail.com>
2023-09-26 23:06:37 +0100

    add Fernet, kdf initialization and encryption/decryption functions to enryptdecrypt module

--- encryptdecrypt.py
+++ encryptdecrypt.py
@@ -1,4 +1,8 @@
+import base64
 from pathlib import Path
+from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from directoryfile import DirectoryFile
 
 
@@ -21,4 +25,34 @@ class EncryptDecrypt(DirectoryFile):
         except FileNotFoundError:
             return None
 
+    def create_kdf(self):
+        salt = b'123qwerty123'
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=32,
+            salt=salt,
+            iterations=390000
+        )
+        return kdf.derive(self.key)
 
+    def create_fernet(self):
+        fernet = Fernet(base64.urlsafe_b64encode(self.create_kdf()))
+        return fernet
+
+    def encrypt(self):
+        fernet = self.create_fernet()
+        encrypted_words = []
+        for text in self.text_from_file().split():
+            encrypted_word = fernet.encrypt(text.encode('utf-8'))
+            encrypted_words.append(encrypted_word)
+
+        return encrypted_words
+
+    def decrypt(self):
+        fernet = self.create_fernet()
+        decrypted_words = []
+        for text in self.encrypt():
+            decrypted_word = fernet.decrypt(text).decode('utf-8')
+            decrypted_words.append(decrypted_word)
+
+        return decrypted_words
