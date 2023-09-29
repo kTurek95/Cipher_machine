7d68f847c038dd2adad364ab3fc1753dafd5d6ab
kTurek95 <k.turek1995@gmail.com>
2023-09-29 22:59:45 +0100

    adding test for get_password function to encrypteddecrypted_tests module

--- encryptdecrypt_tests.py
+++ encryptdecrypt_tests.py
@@ -18,6 +18,21 @@ class TestEncryptedDecrypted(unittest.TestCase):
 
         os.remove(os.path.join('test_password_file.txt'))
 
+    def test_get_password(self):
+        with open('test_password_file.txt', 'w') as file:
+            file.write('kacper95')
+
+        with open('test_password_file.txt') as file:
+            content = file.read()
+
+        ed = EncryptDecrypt('random_directory', content)
+
+        result = ed.get_password()
+
+        self.assertEqual('kacper95', result)
+
+        os.remove(os.path.join('test_password_file.txt'))
+
 
 if __name__ == '__main__':
     unittest.main()
