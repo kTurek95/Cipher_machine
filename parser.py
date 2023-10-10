ef9a73542f8cb9b0a0dca7395dc55aa156410db9
kTurek95 <k.turek1995@gmail.com>
2023-10-10 08:29:42 +0100

    add docstrings to parser.py module

--- parser.py
+++ parser.py
@@ -1,7 +1,22 @@
+"""Module with parser function"""
+
 import argparse
 
 
 def create_parser(args=None):
+    """
+    Create and configure an argument parser.
+
+    This function creates an instance of `argparse.ArgumentParser` and configures it
+    with the necessary command-line arguments.
+
+    Args:
+        args (list): A list of command-line arguments to parse. If not provided,
+                     command-line arguments will be parsed from `sys.argv`.
+
+    Returns:
+        argparse.Namespace: An object containing the parsed command-line arguments.
+    """
     parser = argparse.ArgumentParser(
         description='You can use this program for secure message sending',
         formatter_class=argparse.RawTextHelpFormatter)
