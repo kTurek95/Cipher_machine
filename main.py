5ea5a339b40fc4ac164c5f307812a64b09db7bc7
kTurek95 <k.turek1995@gmail.com>
2023-10-11 13:30:43 +0100

    adding proces_mode function

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os.path
+
+
 def set_password_if_not_set(directory):
     saved_password = directory.get_password()
 
@@ -7,3 +10,27 @@ def set_password_if_not_set(directory):
         return
 
     return saved_password
+
+
+def process_mode(parser, directory, saved_password):
+    entered_password = parser.password
+
+    if entered_password == saved_password:
+        try:
+            if parser.mode == 'encrypt':
+                directory.save_encrypted_text()
+            elif parser.mode == 'decrypt':
+                if os.path.exists('result/encrypted_file.txt'):
+                    directory.save_decrypted_text()
+                else:
+                    print('Nie ma pliku z zaszyfrowanym tekstem')
+            elif parser.mode == 'append':
+                text = input('Podaj jakis wyraz: ')
+                file = input('Podaj plik: ')
+                print(directory.append_text_to_file(text, file))
+            else:
+                raise Exception('Nieznany tryb')
+        except Exception as error:
+            print(str(error))
+    else:
+        print('Nieprawidłowe hasło')
