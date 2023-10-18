79bb358f5971bd4f152cbe223a4b9e6fbac571b6
kTurek95 <k.turek1995@gmail.com>
2023-10-18 18:16:00 +0100

    Refactor tests due to changes in create_password function: - Minor changes in test_create_password due to refactoring of the create_password function in parser.py. - Added test functions: test_without_password_flag and test_password_prompt.

--- parser_tests.py
+++ parser_tests.py
@@ -9,7 +9,7 @@ from parser import create_parser
 class TestCreateParser(unittest.TestCase):
     """ Test case for the create_parser function."""
     @patch('argparse._sys.argv',
-           ['main.py', '-m', 'encrypt', '-p', 'your_password', '-d', 'your_directory'])
+           ['main.py', '-m', 'encrypt', '-ap', 'access_password', '-d', 'your_directory'])
     def test_create_parser(self):
         """
         This test case checks if the create_parser function
@@ -26,12 +26,36 @@ class TestCreateParser(unittest.TestCase):
         self.assertIsInstance(args, argparse.Namespace)
 
         self.assertTrue(hasattr(args, 'mode'))
-        self.assertTrue(hasattr(args, 'password'))
-        self.assertTrue(hasattr(args, 'directoryfile'))
+        self.assertTrue(hasattr(args, 'accessPassword'))
+        self.assertTrue(hasattr(args, 'directoryFile'))
 
         self.assertEqual(args.mode, 'encrypt')
-        self.assertEqual(args.password, 'your_password')
-        self.assertEqual(args.directoryfile, 'your_directory')
+        self.assertEqual(args.accessPassword, 'access_password')
+        self.assertEqual(args.directoryFile, 'your_directory')
+
+    def test_without_password_flag(self):
+        """
+        Test if the password flag is not set by default.
+
+        When the program is invoked without the '-p' flag, it's expected
+        that args.password will be False.
+        """
+        args = create_parser()
+        with patch('sys.argv', ['script_name']):
+            self.assertFalse(args.password)
+
+    @patch('getpass.getpass', return_value="test_password")
+    def test_password_prompt(self, mock_getpass):
+        """
+        Test the program's response to the presence of the '-p' flag.
+
+        When the program is invoked with the '-p' flag, it's expected
+        that args.password will be True and the function to get the
+        password from the user will be called.
+        """
+        with patch('sys.argv', ['script_name', '-p']):
+            args = create_parser()
+            self.assertTrue(args.password)
 
 
 if __name__ == '__main__':
